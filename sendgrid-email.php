4624d00236bd76588f0e539ad74ac9987acf120e
Eduardo Almeida <eduardoalmeida258@gmail.com>
2017-07-26 18:08:14 -0300

    POO

--- sendgrid-email.php
+++ sendgrid-email.php
@@ -2,59 +2,63 @@
 
     // autoload composer
     require 'vendor/autoload.php';
-    
 
-    // Headers for CROSS
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+    // Create a class for Send Email for Send Grid
+    class SendMailSendGrid{
+        private $name;        
+        private $email;
+        private $subject;
+        private $content;
+        private $attachment;
+        private $emailFor;
+        private $captcha;
+        
+        private $apiKey;
 
+        function __construct($apiKey){
+            $this->apiKey = $apiKey;
+        }
+        
+        function sendFor($emailFor){
+            $this->emailFor = $emailFor;
+        }
 
-    // get Data Post
-    $enviaPara = $_POST['send'];
-    $nome = $_POST['Nome'];
-    $email = $_POST['Email'];
-    $telefone = $_POST['Telefone'];
-    $assunto = "VIA SITE: " . $_POST['Assunto'];
-    $domain = preg_replace('/^www\./', '', trim($_POST['domain']));;
-    $mensagem = $_POST['Inf_gerais'];
+        function sendFields($name, $email, $subject, $content){
+            $this->name = $name;
+            $this->email = $email;
+            $this->subject = $subject;
+            $this->content = $content;
+        }
 
-    $content_html .= "Olá! <strong>".$nome."</strong> enviou uma mensagem.";
-    $content_html .= "<br /><br /><strong>Nome Completo:</strong> ".$nome."<br>";
-    $content_html .= "<strong>Email:</strong> ".$email."<br>";
-    $content_html .= "<strong>Telefone:</strong> ".$telefone."<br>";
-    $content_html .= "<strong>Assunto:</strong> ".$assunto."<br>";
-    $content_html .= "<strong>Mensagem:</strong> ".$mensagem."<br>";
+        function send(){
 
-    if ($domain) {
-        $content_html .= "<br><br><small>Mensagem enviada de: </small>" . $domain;
-    }
+            $from = new SendGrid\Email("CONTATO", "contato");
 
-    // Configurando Dados
-    $from = new SendGrid\Email("CONTATO", "contato");
-    $subject = $assunto;
-    $to = new SendGrid\Email($nome, $enviaPara);
-    $content = new SendGrid\Content("text/html", $content_html);
-    $mail = new SendGrid\Mail($from, $subject, $to, $content);
+            $to = new SendGrid\Email($this->name, $this->sendFor);
 
-    // Secret KEY
-    $apiKey = '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';
+            $content = new SendGrid\Content("text/html", $this->content);
 
-    $sg = new \SendGrid($apiKey);
+            $mail = new SendGrid\Mail($from, $this->subject, $to, $content);
 
-    $mail = new SendGrid\Mail($from, $subject, $to, $content);
-    
-    $response = $sg->client->mail()->send()->post($mail);
+            $sg = new \SendGrid($this->apiKey);
 
-    if($response->statusCode() != 202){
-         $return = array(
-            'success' => false,
-            'status' => $response->statusCode()
-        );
-        echo json_encode($return);
-    }else{
-        $return = array(
-            'success' => true,
-            'status' => $response->statusCode()
-        );
-        echo json_encode($return);
+            $mail = new SendGrid\Mail($from, $this->subject, $to, $content);
+            
+            $response = $sg->client->mail()->send()->post($mail);
+            
+            if($response->statusCode() != 202){
+                $return = array(
+                    'success' => false,
+                    'status' => $response->statusCode()
+                );
+                echo json_encode($return);
+            }else{
+                $return = array(
+                    'success' => true,
+                    'status' => $response->statusCode()
+                );
+                echo json_encode($return);
+            }
+        }
+
     }
